a1be908543495691b2765f377e473fbf81b836d9
Chetan Kale <ragineekale@gmail.com>
2021-05-01 00:01:29 +0530

    Changed user table script

--- src/main/resources/filmbuff_scripts/FILMBUFF_CREATE_USER.sql
+++ src/main/resources/filmbuff_scripts/FILMBUFF_CREATE_USER.sql
@@ -6,7 +6,6 @@ CREATE TABLE `filmbuff`.`user` (
   `firstname` VARCHAR(45) NULL,
   `lastname` VARCHAR(45) NULL,
   `email` VARCHAR(50) NULL,
-  `username` VARCHAR(45) NULL,
   `password` VARCHAR(100) NULL,
-  `enabled` TINYINT(1) NULL,
+  `enabled` TINYINT NULL,
   PRIMARY KEY (`id`));
